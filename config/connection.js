07d72c1e2bd025d67ccf6bcaabcfef99ecce2432
Wayne Cheng <wayne@che.ng>
2017-07-27 13:40:23 -0700

    added JawsDB

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,23 @@
 (function(){
 //==================================================
 	var mysql = require("mysql");
-
+	var connection;
 	// Connection Config
-	var connection = mysql.createConnection({
-		port: 3306,
-		host: "localhost",
-		user: "wayne",
-		password: "batman",
-		database: "auth_db"
-	});
+	
+	// JawsDB by default
+	if (process.env.JAWSDB_URL){
+		connection = mysql.createConnection(process.env.JAWSDB_URL)
+	}
+	// local mysql if there is no JawsDB
+	else {
+		connection = mysql.createConnection({
+			port: 3306,
+			host: "localhost",
+			user: "root",
+			password: "",
+			database: "auth_db"
+		});
+	}
 
 	// Make connection.
 	connection.connect(function(err) {
