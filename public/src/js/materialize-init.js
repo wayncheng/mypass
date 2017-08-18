d8162bd9c2d46967a126c99854991741733a0ae6
Wayne Cheng <wayne@che.ng>
2017-08-18 14:04:25 -0700

    style fine tuning

--- public/src/js/materialize-init.js
+++ public/src/js/materialize-init.js
@@ -8,4 +8,11 @@ $('.carousel.carousel-slider').carousel({
 });
 
 
-$(".button-collapse").sideNav();
+$(".button-collapse").sideNav({
+	menuWidth: 300, // Default is 300
+	edge: 'right', // Choose the horizontal origin
+	closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+	draggable: true, // Choose whether you can drag to open on touch screens,
+	onOpen: function(el) { }, // A function to be called when sideNav is opened
+	onClose: function(el) { }, // A function to be called when sideNav is closed
+});
